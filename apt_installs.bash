3c22b28092f865095bf920bfa543b24a2df13cb5
Olivier Cloux <olivier.cloux@epfl.ch>
2020-04-21 15:21:30 +0200

    more apt (un)install

--- apt_installs.bash
+++ apt_installs.bash
@@ -1,2 +1,2 @@
 sudo apt update
-sudo apt install -y vim openssh-server openssh-client thunderbird htop nethogs iotop nmap git vlc chromium gimp
+sudo apt install -y vim openssh-server openssh-client thunderbird thunderbird-l10n-fr htop nethogs iotop nmap git vlc chromium gimp rsync audacity zathura okular inkscape picard k3b nextcloud-desktop ffmpeg firmware-iwlwifi network-manager-openconnect network-manager-ssh network-manager-openconnect-gnome network-manager-ssh-gnome firmware-linux-nonfree texlive-base texlive-science texlive-latex-extra sshfs pdftk latexmk
---
+++ apt_uninstall.bash
@@ -0,0 +1,2 @@
+sudo apt purge gnome-characters gnome-documents gnome-games gnome-klotski gnome-robots gnome-sudoku gnome-weather gnome-todo gnome-sushi gnome-tetravex totem evolution evince five-or-more tali yelp
+sudo apt autoremove
