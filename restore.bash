44fb456000ce9b57588988ba676aeb365dc0b32f
Olivier Cloux <olivier.cloux@epfl.ch>
2020-04-21 15:18:28 +0200

    simplified restore script

--- restore.bash
+++ restore.bash
@@ -1,2 +1,4 @@
 # /opt
-sudo tar -xvf /media/battleman/Lacie/Backup_w540_debian_20200416_1600.tar.gz -C /opt/ --strip-components=2 --wildcards 78d47bf8-fcfb-4eb8-8bab-14fdab8196a0/opt/*	
+src=/media/battleman/Lacie/Backup_w540_debian_20200416_1600.tar.gz 
+
+sudo tar src -xvf -C / --strip-components=1 --wildcards --files-from restore.txt
